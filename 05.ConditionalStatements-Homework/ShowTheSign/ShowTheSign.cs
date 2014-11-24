43255ae01b6e85cf4f159554d2d948b57eb56a88
simoto <sime679@gmail.com>
2014-11-24 22:04:13 +0200

    Added Conditional Statements homework

---
+++ 05.ConditionalStatements-Homework/ShowTheSign/ShowTheSign.cs
@@ -0,0 +1,41 @@
+﻿//Write a program that shows the sign (+ or -) of the product of three real numbers without calculating it. Use a sequence of if statements.
+
+using System;
+
+class ShowTheSign
+{
+    static void Main()
+    {
+        Console.WriteLine("Please enter first number: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter second number: ");
+        double b = double.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter third number: ");
+        double c = double.Parse(Console.ReadLine());
+
+        int signCounter = 1;
+
+        if (a < 0)
+        {
+            signCounter++;
+        }
+
+        if (b < 0)
+        {
+            signCounter++;
+        }
+        if (c < 0)
+        {
+            signCounter++;
+        }
+
+        if (signCounter % 2 == 1)
+        {
+            Console.WriteLine("The sign of the product of the three numbers is +");
+        }
+        else
+        {
+            Console.WriteLine("The sign of the product of the three numbers is -");
+        }
+    }
+}
---
+++ 05.ConditionalStatements-Homework/SortThreeNumbersInDescending/SortThreeNumbersInDescending.cs
@@ -0,0 +1,62 @@
+﻿//Sort 3 real values in descending order using nested if statements.
+
+using System;
+
+class SortThreeNumbersInDescending
+{
+    static void Main()
+    {
+        Console.WriteLine("a:");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("b:");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("c:");
+        int c = int.Parse(Console.ReadLine());
+
+        if (a < b)
+        {
+            if (c < a)
+            {
+                Console.WriteLine("c = {0} is the smallest", c);
+                Console.WriteLine("a = {0} is the middle", a);
+                Console.WriteLine("b = {0} is the biggest", b);
+            }
+            else if (c < b)
+            {
+                Console.WriteLine("a = {0} is the smallest", a);
+                Console.WriteLine("c = {0} is the middle", c);
+                Console.WriteLine("b = {0} is the biggest", b);
+            }
+            else
+            {
+                Console.WriteLine("a = {0} is the smallest", a);
+                Console.WriteLine("b = {0} is the middle", b);
+                Console.WriteLine("c = {0} is the biggest", c);
+            }
+        }
+        else if (b < c)
+        {
+            if (a < c)
+            {
+                Console.WriteLine("b = {0} is the smallest", b);
+                Console.WriteLine("a = {0} is the middle", a);
+                Console.WriteLine("c = {0} is the biggest", c);
+            }
+            else
+            {
+                Console.WriteLine("b = {0} is the smallest", b);
+                Console.WriteLine("c = {0} is the middle", c);
+                Console.WriteLine("a = {0} is the biggest", a);
+            }
+        }
+        else if (c < a)
+        {
+            if (c < b)
+            {
+                Console.WriteLine("c = {0} is the smallest", c);
+                Console.WriteLine("b = {0} is the middle", b);
+                Console.WriteLine("a = {0} is the biggest", a);
+            }
+        }
+    }
+}
