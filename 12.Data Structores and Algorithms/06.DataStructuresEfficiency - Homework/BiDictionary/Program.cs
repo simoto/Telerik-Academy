2303ba90e745ee61f8a4461b1880704c09adb9e5
simoto <simeon.d.cholakov@gmail.com>
2015-03-25 23:17:59 +0200

    added BiDictionary for DataStructuresEfficiency homework

---
+++ 12.Data Structores and Algorithms/06.DataStructuresEfficiency - Homework/BiDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace BiDictionary
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new BiDictionary<string, int, string>(allowDuplicateValues: true);
+            dict.Add("net", 1, "ASP.NET");
+            dict.Add("asp", 2, "ASP.NET");
+            dict.Add("mean", 3, "JavaScript");
+            dict.Add("mean", 3, "Mongo");
+            dict.Add("laravel", 3, "PHP");
+            dict.Add("laravel", 1, "Framework");
+
+            Console.WriteLine(string.Join(" ", dict.GetByFirstKey("net")));
+            Console.WriteLine(string.Join(" ", dict.GetBySecondKey(3)));
+            Console.WriteLine(string.Join(" ", dict.GetByFirstAndSecondKey("mean", 3)));
+            Console.WriteLine(dict.Count);
+
+            dict.RemoveByFirstKey("laravel");
+            Console.WriteLine(dict.Count);
+            dict.RemoveBySecondKey(3);
+            Console.WriteLine(dict.Count);
+            dict.RemoveByFirstAndSecondKey("net", 1);
+            Console.WriteLine(dict.Count);
+        }
+    }
+}
